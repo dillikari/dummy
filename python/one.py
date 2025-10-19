8f734987bf2c0eab0aba62b98ebcd424010fa612
Dilli Babu <dillimk25@gmail.com>
2025-10-19 19:58:44 +0530

    changes

--- python/one.py
+++ python/one.py
@@ -1,6 +1,11 @@
-a=10
-b=20
-c=a+b
-print(a)
-print(b)
-print(c)
+def one(a):
+    b=10
+    def two(c):
+        nonlocal b
+        result=a+b+c
+        print("a={},b={},c={}=sum={}".format(a,b,c,result))
+        b+=1
+    return two
+result=one(10)
+for val in range(1,5):
+    result(val)
